df6933fb91ded0a5ad3cb169e8d3fb18ab68214f
shad <105544140+shadmbwagha@users.noreply.github.com>
2023-06-05 12:47:41 -0700

    a

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './login/login.component';
 import { Route, RouterModule, Routes } from '@angular/router';
-import { UserComponent } from './user/user.component';
+
 import { ErrorComponent } from './error/error.component';
 import { ProfileDetailsComponent } from './profile-details/profile-details.component';
 
