628d35febe1e743a0451169c5082d04c1b6c3828
shad <105544140+shadmbwagha@users.noreply.github.com>
2023-06-22 11:33:03 -0700

    Revert "last procument"
    This reverts commit c3ce8fc510684a7a92a8be197504caaac678f1c0.

--- src/token-interceptor.service.ts
+++ src/token-interceptor.service.ts
@@ -12,7 +12,7 @@ import { Observable } from 'rxjs';
 })
 export class TokenInterceptorService implements HttpInterceptor{
 
-  auth = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJzaGFkcmFja0BqaXRlZ2VtZWVob2xkaW5ncy5jby50eiIsInJvbGVzIjpbIlVTRVIiLCJBRE1JTiJdLCJleHAiOjE2ODc1NDQxNjgsImlhdCI6MTY4NzQ1Nzc2OH0.0p5PXWgqvKzkWMYaMLc5EPsWqPAUnxMjR935mc4dD10'
+  auth = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJzaGFkcmFja0BqaXRlZ2VtZWVob2xkaW5ncy5jby50eiIsInJvbGVzIjpbIlVTRVIiLCJBRE1JTiJdLCJleHAiOjE2ODcyNTE1NjksImlhdCI6MTY4NzE2NTE2OX0.nsesNq5Cz1x-RPrUFIqR8t91S0Or2y5yjQtVS4dvgc8'
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
     request = request.clone({
